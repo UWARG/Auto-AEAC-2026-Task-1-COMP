fffdec93c3519256be31174fcf108a65c02cb16f
Vincent <vincentpersonal1111@gmail.com>
2026-01-23 21:16:12 -0500

    Implemented MavlinkReceiver Class

---
+++ groundstation/MavlinkReceiver.py
@@ -0,0 +1,40 @@
+import time
+import logging
+from pymavlink import mavutil
+
+
+class MavlinkReciever:
+
+    def __init__(self, connection_string: str) -> mavutil.mavlink_connection:
+        while not self.__mavlink_connected(connection_string):
+            time.sleep(1)
+
+    def __mavlink_connected(self, connection_string:str) -> bool:
+        try:
+            self.connection = mavutil.mavlink_connection(connection_string)
+            logging.info(f"Connected to {self.connection.target_system}"
+                         f", component {self.connection.target_component}")
+            self.connection.wait_heartbeat()
+            logging.info("Received heartbeat")
+            return True
+        except Exception as e:
+            logging.error("Encountered Error {e}. Trying Again")
+            return False
+
+    def get_message(self) -> tuple[True, list[str]] | tuple[bool, None]:
+        msg = self.connection.recv_match(type="STATUSTEXT", blocking=True)
+        if not msg:
+            logging.error("Recieved empty message")
+            return False, None
+        if msg.get_type() == "BAD_DATA":
+            logging.error("Received bad data")
+            return False, None
+        try:
+            text = msg.text
+            if isinstance(text, bytes):
+                message = text.decode("UTF-8").strip("\x00")
+                info = message.split(",")
+                return True, info
+        except Exception as e:
+            logging.error("message processing failed {e}")
+        return False, None
--- groundstation/main.py
+++ groundstation/main.py
@@ -1,70 +1,52 @@
-from pymavlink import mavutil
-from common.modules.logger import logger
-from common.modules.logger import logger_main_setup
-from common.modules.read_yaml import read_yaml
+import logging
+from groundstation.MavlinkReceiver import MavlinkReciever
 
-CONNECTION_STRING = "tcp.localhost:1400"
+
+CONNECTION_STRING = "/dev/ttyAMA0"
 
 
 def main() -> None:
-    success, config = read_yaml.open_config(logger.CONFIG_FILE_PATH)
-    if not success:
-        print("Could not read config file")
-    assert success is True
-    success, main_logger, _ = logger_main_setup.setup_main_logger(config)
-    if not success:
-        print("Could not create main_logger")
-    assert success is True
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    receiver = MavlinkReciever(CONNECTION_STRING)
+    try:
+        while True:
+            success, info = receiver.get_message()
+            if not success:
+                continue
+            # direction
+            if info[0] == "n":
+                direction = "North"
+            elif info[0] == "s":
+                direction = "South"
+            elif info[0] == "w":
+                direction = "West"
+            elif info[0] == "e":
+                direction = "East"
+            # color
+            if info[1] == "r":
+                color = "red"
+            elif info[1] == "b":
+                color = "blue"
+            elif info[1] == "y":
+                color = "yellow"
+            elif info[1] == "g":
+                color = "green"
+            # amount up and to the right of bottom left corner of wall
+            up = info[2]
+            right = info[3]
+            # Log a human readable message
+            logging.info(
+                f"The {color} target is {up} units up and {right} units"
+                f" right from the bottom left corner of the {direction}"
+                f" wall"
+            )
 
-    connection = mavutil.mavlink_connection(CONNECTION_STRING)
-    connection.wait_heartbeat()
-    while True:
-        msg = connection.recv_match(type="STATUSTEXT", blocking=True)
-        if not msg:
-            main_logger.error("Recieved empty message")
-            continue
-        if msg.get_type() == "BAD_DATA":
-            main_logger.error("Recieved bad data")
-            continue
-        try:
-            text = msg.text
-            if isinstance(text, bytes):
-                message = text.decode("UTF-8").strip("\x00")
-                info = message.split(",")
-                # direction
-                if info[0] == "n":
-                    direction = "North"
-                elif info[0] == "s":
-                    direction = "South"
-                elif info[0] == "w":
-                    direction = "West"
-                elif info[0] == "e":
-                    direction = "East"
-                # color
-                if info[1] == "r":
-                    color = "red"
-                elif info[1] == "b":
-                    color = "blue"
-                elif info[1] == "y":
-                    color = "yellow"
-                elif info[1] == "g":
-                    color = "green"
-                # amount up and to the right of bottom left corner of wall
-                up = info[2]
-                right = info[3]
-                # Log a human readable message
-                main_logger.info(
-                    f"The {color} target is {up} units up and {right} units"
-                    f" right from the bottom left corner of the {direction}"
-                    f" wall"
-                )
-
-        except AttributeError:
-            main_logger.error('Could not find property "text"')
-        except UnicodeDecodeError:
-            main_logger.error("Could not decode text")
-        except IndexError:
-            main_logger.error("Error in message's data")
+    except IndexError:
+        logging.error("Error in Message's data")
+    except KeyboardInterrupt:
+        print("Terminating Program")
 
 
 if __name__ == "__main__":

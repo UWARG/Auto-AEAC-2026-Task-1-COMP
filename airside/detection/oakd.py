57e17a92346339720c22af45b5adbe66b3aa6d60
honzikschenk <nojschenk@gmail.com>
2026-02-07 00:00:43 -0500

    Adds oakd poilerplate

---
+++ airside/detection/oakd.py
@@ -0,0 +1,9 @@
+from util import MappedTarget
+
+class OakD:
+    def __init__(self):
+        pass
+
+    # Returns a list of detected targets at that moment
+    def run(self) -> list[MappedTarget]:
+        return []
--- airside/main.py
+++ airside/main.py
@@ -2,11 +2,10 @@
 
 import logging
 
+from airside.detection.oakd import OakD
 from airside.post_processing import post_processing
 from util import Coordinate, Vector3d
 from mavlink_comm import MavlinkComm
-
-Vector3d(0, 0, 0)
 
 def main() -> None:
     """Main control loop for airside drone operations."""
@@ -17,10 +16,15 @@ def main() -> None:
 
     mav_comm = MavlinkComm()
 
+    oakd = OakD()
+
     while mav_comm.post_processing_requested() is False:
         mav_comm.process_data_stream()
+
+        oakd.run()
     
-    post_processing.run("rtab-data.db", mav_comm)
+    # TODO: Feed in the correct database and targets files
+    post_processing.run("rtab-data.db", "targets.txt", mav_comm)
 
 
 if __name__ == "__main__":

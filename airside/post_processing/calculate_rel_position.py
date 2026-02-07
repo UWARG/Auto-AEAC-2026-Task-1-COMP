ea8b5eae146ca167ac5ec115dac8c6dc6a2efec8
Vincent <v23zhou@uwaterloo.ca>
2026-02-07 18:17:22 -0500

    Made changes based on new assumptions and fixed errors

--- airside/post_processing/calculate_rel_position.py
+++ airside/post_processing/calculate_rel_position.py
@@ -2,7 +2,11 @@
 import util as util
 import math
 import numpy as np
+"""
+The leftmost corner of the first plane should have the largest positive y value.
+All other corners can be determined based off this information of the first bottom left corner
 
+"""
 class target_positions:
     DEVIATION=0.01
     ANGLE_THRESHOLD=0.087 #~=5 degrees
@@ -12,64 +16,59 @@ class target_positions:
                   targets:list[util.Target],
                   directions:util.Direction) -> list:
         bottom_corners=[]
-        parallel_to_z={
+        parallel_to_x={
              "perp1":None,
              "perp2":None,
-             "parallel1":None,
+             "parallel1":None, # first wall with cardinal direction
              "parallel2":None
         }
-        ground_vector=np.array([ground.normal.x,
-                        ground.normal.z,
-                        -ground.normal.y]
-                        )
-        parallel_to_z["parallel1"]=np.array([walls[0].normal.x,
-                            walls[0].normal.z,
-                            -walls[0].normal.y])
-        for j in walls[1:]:
-            wall2=np.array([j.normal.x,
-                            j.normal.z,
-                            -j.normal.y])
-            dp=np.dot(parallel_to_z["parallel1"],wall2)
-            angle=math.acos(dp/(np.linalg.norm(parallel_to_z["parallel1"])*np.linalg.norm(wall2)))
-            if (abs(angle)>(math.pi/2+self.ANGLE_THRESHOLD)) or (abs(angle)<self.ANGLE_THRESHOLD):
-                parallel_to_z["parallel2"]=j
-                continue
-            matrix=np.vstack((parallel_to_z["parallel1"],wall2,ground_vector))
-            b=np.array([walls[0].offset,j.offset,ground.offset])
-            corner=np.linalg.solve(matrix,b)
-            print(corner)
-            if not parallel_to_z["parallel1"]:
-                 parallel_to_z["parallel1"]=j
+        for i in walls:
+            wall_normal=np.array([i.normal.x,i.normal.y,i.normal.z])
+            dp=np.dot(wall_normal,np.array([1,0,0]))
+            angle=math.acos(np.clip(dp/(np.linalg.norm(i.normal)*1),-1,1))
+            if abs(angle)<self.ANGLE_THRESHOLD or abs(angle)>math.pi-self.ANGLE_THRESHOLD:
+                if not parallel_to_x["parallel1"]:
+                    parallel_to_x["parallel1"]=i
+                else:
+                    if abs(parallel_to_x["parallel1"].offset)>abs(i.offset):
+                        parallel_to_x["parallel2"]=parallel_to_x["parallel1"]
+                        parallel_to_x["parallel1"]=i #index 0 for object, index 1 for index
+                    else:
+                        parallel_to_x["parallel2"]=i
             else:
-                 parallel_to_z["parallel2"]=j
-            bottom_corners.append(corner)
-        second_wall=parallel_to_z["parallel2"]
-        perp_wall_1=parallel_to_z["perp1"]
-        perp_wall_2=parallel_to_z["perp2"]
+                if not parallel_to_x["perp1"]:
+                    parallel_to_x["perp1"]=i
+                else:
+                    parallel_to_x["perp2"]=i
 
-        parallel_to_z["perp1"]=np.array([perp_wall_1.normal.x,
-                                     perp_wall_1.normal.z,
-                                     -perp_wall_1.normal.y])
-        parallel_to_z["perp2"]=np.array([perp_wall_2.normal.x,
-                                     perp_wall_2.normal.z,
-                                     -perp_wall_2.normal.y])
-        parallel_to_z["parallel2"]=np.array([parallel_to_z["parallel2"].normal.x,
-                               parallel_to_z["parallel2"].normal.z,
-                               -parallel_to_z["parallel2"].normal.y])
-        b=np.array([parallel_to_z["parallel2"].offset,perp_wall_1.offset,ground.offset])
-        bottom_corners.append(np.linalg.solve(
-             np.vstack((parallel_to_z["parallel2"],
-                       parallel_to_z["perp1"],
-                       ground_vector)),
-             b
-        ))
-        b=np.array([second_wall.offset,perp_wall_2.offset,ground.offset])
-        bottom_corners.append(np.linalg.solve(
-             np.vstack((parallel_to_z["parallel2"],
-                       parallel_to_z["perp2"],
-                       ground_vector)),
-             b
-        ))
+        
+        ground_vector=np.array([ground.normal.x,
+                        ground.normal.y,
+                        ground.normal.z]
+                        )
+        wall1=np.array([parallel_to_x["parallel1"].normal.x,
+                            parallel_to_x["parallel1"].normal.y,
+                            parallel_to_x["parallel1"].normal.z])
+        far_wall=np.array([parallel_to_x["parallel2"].normal.x, #far wall parallel to first wall with cardinal direction
+                            parallel_to_x["parallel2"].normal.y,
+                            parallel_to_x["parallel2"].normal.z])
+        for j in [parallel_to_x["perp1"],parallel_to_x["perp2"]]:
+            wall2=np.array([j.normal.x,
+                            j.normal.y,
+                            j.normal.z])
+            matrix=np.vstack((wall1,wall2,ground_vector))
+            matrix2=np.vstack((far_wall,wall2,ground_vector))
+            b=np.array([parallel_to_x["parallel1"].offset,j.offset,ground.offset])
+            b2=np.array([parallel_to_x["parallel2"].offset,j.offset,ground.offset])
+            corner=np.linalg.solve(matrix,b)
+            corner2=np.linalg.solve(matrix2,b2)
+            bottom_corners.append(corner) #front 2 corners
+            bottom_corners.append(corner2) #back 2 corners
+
+
+        
+            
+            
 
     
         

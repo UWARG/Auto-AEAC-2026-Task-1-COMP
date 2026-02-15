be2fbf6c3562fab4d4cee0e204f859900dfeaa95
Vincent <v23zhou@uwaterloo.ca>
2026-02-15 02:40:39 -0500

    Review changes and fix bugs

--- airside/post_processing/target_rel_position.py
+++ airside/post_processing/target_rel_position.py
@@ -7,131 +7,133 @@ def locate_targets(
     planes: list[Plane], targets: list[Target], first_direction: Direction
 ) -> list[MappedTarget]:
 
-    MARGIN = 0.5
+    MARGIN = 0.5 # Margin of error in meteres where anything below this value can be considered negligeble
     ANGLE_THRESHOLD = 0.087  # ~= 5 degrees
-    PARALLEL_THRESHOLD = 0.5
+
     bottom_corners = []
-    parallel_to_x = {
-        "parallel1": None,  # wall with cardinal direction, as first index in ordered dict
-        "perp1": None,
-        "perp2": None,
-        "parallel2": None,
-        "ground": None,
+    walls = {
+        "close_x_wall": None,  # wall with cardinal direction, as first index in ordered dict
+        "negative_y_wall": None,
+        "positive_y_wall": None,
+        "far_x_wall": None,
     }
+    ground_vector=None
+    ground_offset=None
     direction = ["NORTH", "EAST", "SOUTH", "WEST", "NORTH", "EAST", "SOUTH", "WEST"]
-    for i in planes:
-        wall_normal = np.array([i.normal.x, i.normal.y, i.normal.z])
-        if wall_normal[2] > MARGIN:  # if normal has some z component
-            parallel_to_x["ground"] = i
+    for plane in planes:
+        plane_normal = np.array([plane.normal.x, plane.normal.y, plane.normal.z])
+        if plane_normal[2] > MARGIN:  # if normal has some z component
+            ground_vector=plane_normal
+            ground_offset=plane.offset
             continue
-        dp = np.dot(wall_normal, np.array([1, 0, 0]))
-        angle = math.acos(np.clip(dp / (np.linalg.norm(wall_normal) * 1), -1, 1))
-        if angle < ANGLE_THRESHOLD or angle > math.pi - ANGLE_THRESHOLD:
-            if not parallel_to_x["parallel1"]:
-                parallel_to_x["parallel1"] = i
+        dp = np.dot(plane_normal, np.array([1, 0, 0]))
+        angle = math.acos(np.clip(dp / (np.linalg.norm(plane_normal)), -1, 1))
+        if angle < ANGLE_THRESHOLD:
+            if not walls["close_x_wall"]:
+                walls["close_x_wall"] = plane
             else:
-                if abs(parallel_to_x["parallel1"].offset) > abs(i.offset):
-                    parallel_to_x["parallel2"] = parallel_to_x["parallel1"]
-                    parallel_to_x["parallel1"] = i
+                if abs(walls["close_x_wall"].offset) > abs(plane.offset):
+                    walls["far_x_wall"] = walls["close_x_wall"]
+                    walls["close_x_wall"] = plane
                 else:
-                    parallel_to_x["parallel2"] = i
+                    walls["far_x_wall"] = plane
         # Want perp1 to be the wall in the positive y direction
         else:
-            if not parallel_to_x["perp1"]:
-                parallel_to_x["perp1"] = i
+            if not walls["negative_y_wall"]:
+                walls["negative_y_wall"] = plane
             else:
                 if (
-                    parallel_to_x["perp1"].offset / parallel_to_x["perp1"].normal.y
-                    < i.offset / i.normal.y
+                    plane.normal.y>0
                 ):  # we only have to divide by normal.y for perp walls since their normals are opposite direction
-                    parallel_to_x["perp2"] = i
+                    walls["positive_y_wall"] = plane
                 else:
-                    parallel_to_x["perp2"] = parallel_to_x["perp1"]
-                    parallel_to_x["perp1"] = i
+                    walls["positive_y_wall"] = walls["negative_y_wall"]
+                    walls["negative_y_wall"] = plane
 
-    ground_vector = np.array(
-        [
-            parallel_to_x["ground"].normal.x,
-            parallel_to_x["ground"].normal.y,
-            parallel_to_x["ground"].normal.z,
-        ]
-    )
     close_wall = np.array(
         [
-            parallel_to_x["parallel1"].normal.x,
-            parallel_to_x["parallel1"].normal.y,
-            parallel_to_x["parallel1"].normal.z,
+            walls["close_x_wall"].normal.x,
+            walls["close_x_wall"].normal.y,
+            walls["close_x_wall"].normal.z,
         ]
     )
     far_wall = np.array(
         [
-            parallel_to_x[
-                "parallel2"
+            walls[
+                "far_x_wall"
             ].normal.x,  # far wall parallel to first wall with cardinal direction
-            parallel_to_x["parallel2"].normal.y,
-            parallel_to_x["parallel2"].normal.z,
+            walls["far_x_wall"].normal.y,
+            walls["far_x_wall"].normal.z,
         ]
     )
-    for j in [parallel_to_x["perp1"], parallel_to_x["perp2"]]:
-        wall2 = np.array([j.normal.x, j.normal.y, j.normal.z])
+    for wall in [walls["negative_y_wall"], walls["positive_y_wall"]]:
+        wall2 = np.array([wall.normal.x, wall.normal.y, wall.normal.z])
         matrix = np.vstack((close_wall, wall2, ground_vector))
         matrix2 = np.vstack((far_wall, wall2, ground_vector))
         b = np.array(
             [
-                parallel_to_x["parallel1"].offset,
-                j.offset,
-                parallel_to_x["ground"].offset,
+                walls["close_x_wall"].offset,
+                wall.offset,
+                ground_offset,
             ]
         )
         b2 = np.array(
             [
-                parallel_to_x["parallel2"].offset,
-                j.offset,
-                parallel_to_x["ground"].offset,
+                walls["far_x_wall"].offset,
+                wall.offset,
+                ground_offset,
             ]
         )
         corner = np.linalg.solve(matrix, b)
         corner2 = np.linalg.solve(matrix2, b2)
-        bottom_corners.append(corner)  # front corner
-        bottom_corners.append(corner2)  # back corner
+        #order corners to go clockwise as index increases
+        if wall.normal.y<0:
+            bottom_corners.append(corner)  # front corner
+            bottom_corners.append(corner2)  # back corner
+        else:
+            bottom_corners.append(corner2) #back corner
+            bottom_corners.append(corner) #front corner
 
-    target_locations = []
-    for i in targets:
+    target_locations = [] #j=corner, wall=k
+    for target in targets:
         right, up = 0, 0
         target_on_wall = False
-        target = np.array([i.location.x, i.location.y, i.location.z])
-        for j, k in zip(bottom_corners, list(parallel_to_x.values())[0:4]):
-            vector = np.subtract(target, j)
-            normal_vector = np.array([k.normal.x, k.normal.y, k.normal.z])
+        target_vector = np.array([target.location.x, target.location.y, target.location.z])
+        for corner, wall in zip(bottom_corners, list(walls.values())[0:4]):
+            vector = np.subtract(target_vector, corner)
+            normal_vector = np.array([wall.normal.x, wall.normal.y, wall.normal.z])
             dp = np.dot(vector, normal_vector)
             angle = math.acos(
                 dp / (np.linalg.norm(vector) * np.linalg.norm(normal_vector))
             )
+
             if (
-                angle > math.pi / 2 - ANGLE_THRESHOLD
-                and angle < math.pi / 2 + ANGLE_THRESHOLD
+                abs(math.pi/2 - angle) < ANGLE_THRESHOLD
             ):
-                if abs(vector[0]) < PARALLEL_THRESHOLD or abs(vector[0]) < abs(
-                    vector[1]
+                if (abs(vector[0]) < MARGIN or abs(vector[0]) < abs(
+                    vector[1]) 
                 ):
+
                     right, up = abs(vector[1]), abs(vector[2])
-                    relative_position = Coordinate(0, right, up)
+                    relative_position = Coordinate(right, up, 0)
                 else:
                     right, up = abs(vector[0]), abs(vector[2])
-                    relative_position = Coordinate(right, 0, up)
-
+                    relative_position = Coordinate(right, up, 0)
+                if vector[2]<min(abs(vector[0]),abs(vector[1])): #more likely to be on the ground so continue
+                    continue
                 target_on_wall = True
                 index = direction.index(first_direction.value)
-                index2 = list(parallel_to_x.values()).index(k)
+                index2 = list(walls.values()).index(wall)
                 wall_direction = direction[index + index2]
                 target_locations.append(
                     MappedTarget(
-                        Colours[i.colour.name.upper()],
+                        Colours[target.colour.name.upper()],
                         relative_position,
                         Direction[wall_direction],
-                        False,
+                        True,
                     )
                 )
+                break
 
             # value from right of bottom left corner will always be in +x or +y dir. Other component direction
             # should be 0 for "vector"
@@ -140,69 +142,81 @@ def locate_targets(
         if not target_on_wall:
             wall = None
             if (
-                i.location.y
-                < parallel_to_x["perp1"].offset / parallel_to_x["perp1"].normal.y
+                target.location.y
+                < walls["negative_y_wall"].offset / walls["negative_y_wall"].normal.y
             ):
-                if i.location.x > parallel_to_x["parallel2"].offset:
+                if target.location.x > walls["far_x_wall"].offset:
                     vector = np.subtract(
-                        target, bottom_corners[1]
-                    )  # bottom left corner for perp1
-                    position = (-abs(vector[0]), abs(vector[1]))
-                    wall = parallel_to_x["perp1"]
-                elif i.location.x < parallel_to_x["parallel1"].offset:
+                        target_vector, bottom_corners[2]
+                    )
+                    position = (abs(vector[1]), abs(vector[0]))
+                    wall = walls["far_x_wall"]
+                elif target.location.x < walls["close_x_wall"].offset:
                     vector = np.subtract(
-                        target, bottom_corners[0]
-                    )  # left corner for parallel 1
-                    position = (-abs(vector[1]), abs(vector[0]))
-                    wall = parallel_to_x["parallel1"]
+                        target_vector, bottom_corners[1]
+                    )
+                    position = (abs(vector[0]), abs(vector[1]))
+                    wall = walls["negative_y_wall"]
                 else:
                     vector = np.subtract(
-                        target, bottom_corners[1]
-                    )  # left corner for perp 1
+                        target_vector, bottom_corners[1]
+                    )
                     position = (abs(vector[0]), abs(vector[1]))
-                    wall = parallel_to_x["perp1"]
+                    wall = walls["negative_y_wall"]
 
             elif (
-                i.location.y
-                > parallel_to_x["perp2"].offset / parallel_to_x["perp2"].normal.y
+                target.location.y
+                > walls["positive_y_wall"].offset / walls["positive_y_wall"].normal.y
             ):
-                if i.location.x > parallel_to_x["parallel2"].offset:
+                if target.location.x > walls["far_x_wall"].offset:
                     vector = np.subtract(
-                        target, bottom_corners[2]
-                    )  # left corner for perp 2
-                    position = (-abs(vector[0]), abs(vector[1]))
-                    wall = parallel_to_x["perp2"]
-                elif i.location.x < parallel_to_x["parallel1"].offset:
-                    vector = np.subtract(
-                        target, bottom_corners[3]
-                    )  # left corner for parallel 2
-                    position = (-abs(vector[1]), abs(vector[0]))
-                    wall = parallel_to_x["parallel2"]
-                else:
-                    vector = np.subtract(target, bottom_corners[2])
+                        target_vector, bottom_corners[3]
+                    )  
                     position = (abs(vector[0]), abs(vector[1]))
-                    wall = parallel_to_x["perp2"]
+                    wall = walls["positive_y_wall"]
+                elif target.location.x < walls["close_x_wall"].offset:
+                    vector = np.subtract(
+                        target_vector, bottom_corners[0]
+                    )  # left corner for parallel 2
+                    position = (abs(vector[1]), abs(vector[0]))
+                    wall = walls["close_x_wall"]
+                else:
+                    vector = np.subtract(target_vector, bottom_corners[3])
+                    position = (abs(vector[0]), abs(vector[1]))
+                    wall = walls["positive_y_wall"]
             # for in between two perpendicular walls
             else:
-                if i.location.x > parallel_to_x["parallel2"].offset:
-                    vector = np.subtract(target, bottom_corners[3])
-                    wall = parallel_to_x["parallel2"]
-                elif i.location.x < parallel_to_x["parallel1"].offset:
-                    vector = np.subtract(target, bottom_corners[0])
-                    wall = parallel_to_x["parallel1"]
-                else:
-                    raise Exception("Invalid target location")
-                position = (abs(vector[1]), abs(vector[0]))
+                if target.location.x > walls["far_x_wall"].offset:
+                    vector = np.subtract(target_vector, bottom_corners[2])
+                    wall = walls["far_x_wall"]
+                    position = (abs(vector[1]), abs(vector[0]))
+                elif target.location.x < walls["close_x_wall"].offset:
+                    vector = np.subtract(target_vector, bottom_corners[0])
+                    wall = walls["close_x_wall"]
+                    position = (abs(vector[1]), abs(vector[0]))
+                #for slight imperfections within margin of error
+                elif (abs(target.location.x-walls["close_x_wall"].offset)<MARGIN or 
+                abs(target.location.x-walls["far_x_wall"].offset)<MARGIN):
+                    vector=np.subtract(target_vector,bottom_corners[0])
+                    vector2=np.subtract(target_vector,bottom_corners[2])
+                    if np.linalg.norm(vector)<np.linalg.norm(vector2):
+                        position=(abs(vector[1]),-abs(vector[0]))
+                        wall=walls["close_x_wall"]
+                    else:
+                        position=(abs(vector2[1]),-abs(vector2[0]))
+                        wall=walls["far_x_wall"]
+                
+                else: #otherwise invalid
+                    raise Exception(f"Invalid target location {target.location}")
 
-            # create new dictionary
             index = direction.index(first_direction.value)
-            index2 = list(parallel_to_x.values()).index(wall)
+            index2 = list(walls.values()).index(wall)
             wall_direction = direction[index + index2]
             relative_position = Coordinate(*position, 0)
 
             target_locations.append(
                 MappedTarget(
-                    Colours[i.colour.name.upper()],
+                    Colours[target.colour.name.upper()],
                     relative_position,
                     Direction[wall_direction],
                     False,
---
+++ airside/post_processing/testing.py
@@ -0,0 +1,33 @@
+from airside.post_processing import target_rel_position
+from util import Plane, Vector3d, Target, MappedTarget, Direction, Coordinate, Colours
+
+
+#Testing values 1
+planes=[]
+planes.append(Plane(Vector3d(x=1,y=0,z=0),2)) #x_near
+planes.append(Plane(Vector3d(x=1,y=0,z=0),7.3)) #x_far
+planes.append(Plane(Vector3d(x=0,y=1,z=0),2)) #y_positive
+planes.append(Plane(Vector3d(x=0,y=-1,z=0),2)) #y_negative
+planes.append(Plane(Vector3d(x=0,y=0,z=1),3)) #ground
+
+targets=[]
+targets.append(Target(colour=Colours.BLACK,location=Coordinate(x=4,y=2.1,z=4)))
+targets.append(Target(colour=Colours.BLUE,location=Coordinate(x=6.7,y=2.1,z=8)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=2,y=2,z=6)))
+targets.append(Target(colour=Colours.YELLOW,location=Coordinate(x=7.3,y=-2,z=6)))
+targets.append(Target(colour=Colours.YELLOW,location=Coordinate(x=7.3,y=0,z=6)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=5,y=-2,z=6)))
+#floor targets
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=8,y=2.5,z=3)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=7.2,y=2.5,z=3)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=7.1,y=1.9,z=3)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=8.1,y=2.5,z=3)))
+targets.append(Target(colour=Colours.RED,location=Coordinate(x=0,y=-2.5,z=3)))
+
+
+
+
+
+MappedTarget=target_rel_position.locate_targets(planes=planes,targets=targets,first_direction=Direction.WEST)
+for i in MappedTarget:
+    print(i)

88374252316989e78968a526dbc78de3ee34b74d
Matthew Chan <matthew.chan0607@gmail.com>
2026-03-03 14:07:57 -0500

    Fix: Fixed logger

--- tests/test_wall_detection.py
+++ tests/test_wall_detection.py
@@ -1,12 +1,14 @@
 import pytest
 from airside.post_processing.wall_detection import find_walls
 from airside.mavlink_comm import MavlinkComm
-
+import logging
 
 def test_wall_detection():
+    
+    main_logger = logging.getLogger("main")
     file = "stereo_vision_data.ply"
 
-    result = find_walls(file, MavlinkComm(main_logger=None))
+    result = find_walls(file, MavlinkComm(main_logger))
 
     expected_walls = 2
     result_walls = len(result)

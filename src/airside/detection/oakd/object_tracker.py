64ede43935cb0c010d32b462e4a7be66814ceeee
honzikschenk <nojschenk@gmail.com>
2026-03-03 20:38:20 -0500

    Converts to using spacial coordinate layout

--- src/airside/detection/oakd/object_tracker.py
+++ src/airside/detection/oakd/object_tracker.py
@@ -1,51 +1,25 @@
 #!/usr/bin/env python3
 
+from typing import Optional
 import depthai as dai
 from airside.detection.oakd.camera_bundle import CameraBundle
 
-fullFrameTracking = False
-
-# Create pipeline
-
-
-def add_object_tracker(p: dai.Pipeline, cameras: "CameraBundle" = None):
-
+def add_object_tracker(p: dai.Pipeline, cameras: Optional["CameraBundle"] = None):
     cameras = cameras or CameraBundle(p)
-    camRgb = cameras.camRgb
-    stereo = cameras.stereo
+    depthNode = cameras.stereo
+    depthNode.setExtendedDisparity(True)
+    depthNode.setOutputSize(640, 400)
 
     spatialDetectionNetwork = p.create(dai.node.SpatialDetectionNetwork).build(
-        camRgb, stereo, "yolov6-nano"
+        cameras.camRgb, depthNode, dai.NNModelDescription("yolov6-nano")
     )
-    objectTracker = p.create(dai.node.ObjectTracker)
 
     spatialDetectionNetwork.setConfidenceThreshold(0.6)
-    #spatialDetectionNetwork.input.setQueueSize(1)
     spatialDetectionNetwork.input.setBlocking(False)
     spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
     spatialDetectionNetwork.setDepthLowerThreshold(100)
     spatialDetectionNetwork.setDepthUpperThreshold(5000)
 
-    objectTracker.setDetectionLabelsToTrack([0])  # track only person
-    # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
-    objectTracker.setTrackerType(dai.TrackerType.SHORT_TERM_IMAGELESS)
-    # take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
-    objectTracker.setTrackerIdAssignmentPolicy(
-        dai.TrackerIdAssignmentPolicy.SMALLEST_ID
-    )
-
-    if fullFrameTracking:
-        camRgb.requestFullResolutionOutput().link(objectTracker.inputTrackerFrame)
-        # do not block the pipeline if it's too slow on full frame
-        objectTracker.inputTrackerFrame.setBlocking(False)
-        objectTracker.inputTrackerFrame.setMaxSize(1)
-    else:
-        spatialDetectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
-
-    spatialDetectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
-    spatialDetectionNetwork.out.link(objectTracker.inputDetections)
-    # create output queues to read tracking results
-    qTracklets = objectTracker.out.createOutputQueue(maxSize=4, blocking=False)
-    qFrame = objectTracker.passthroughTrackerFrame.createOutputQueue(maxSize=4, blocking=False)
-    return qTracklets, qFrame
-
+    qDetections = spatialDetectionNetwork.out.createOutputQueue(maxSize=16, blocking=False)
+    qFrame = spatialDetectionNetwork.passthrough.createOutputQueue(maxSize=16, blocking=False)
+    return qDetections, qFrame

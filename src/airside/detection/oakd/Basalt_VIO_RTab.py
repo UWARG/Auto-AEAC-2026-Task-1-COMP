1a39a297598ebb17baf95ab9dcd707adf1260f56
Adityya-K <adityyakaushal@gmail.com>
2026-03-07 22:32:59 -0500

    fix: CI/CD

--- src/airside/detection/oakd/Basalt_VIO_RTab.py
+++ src/airside/detection/oakd/Basalt_VIO_RTab.py
@@ -17,10 +17,10 @@ def add_basalt_vio_rtab(p: dai.Pipeline, cameras: Optional[CameraBundle] = None)
         odom = p.create(dai.node.BasaltVIO)
         stereo = cameras.stereo
         slam = cameras.slam
-        
+
         db_path = output_folder / "building.db"
         db_path.unlink(missing_ok=True)
-        
+
         slam.setDatabasePath(str(db_path))
         params = {
             "RGBD/CreateOccupancyGrid": "true",

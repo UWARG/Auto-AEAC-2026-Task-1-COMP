471a46a02e3691517537147ed6e31686c404b7ae
honzikschenk <nojschenk@gmail.com>
2026-03-08 23:16:47 -0600

    Updates arducam to match oakd instantiation and adds more safety to mavlink_comm

--- src/airside/detection/arducam.py
+++ src/airside/detection/arducam.py
@@ -1,5 +1,6 @@
 import threading
 import logging
+import time
 
 from .abstract_camera import AbstractCamera
 from util import MappedTarget, Direction, Coordinate, Colours
@@ -10,12 +11,14 @@ class Arducam(AbstractCamera):
         self,
         main_logger: logging.Logger,
         detections_logger: logging.Logger,
+        detailed_detections_logger: logging.Logger,
         stop_event: threading.Event,
     ):
-        super().__init__(main_logger, detections_logger, stop_event)
+        super().__init__(main_logger, detections_logger, detailed_detections_logger, stop_event)
 
     def run(self):
         while not self.stop_event.is_set():
+            time.sleep(1)
             mapped_target = MappedTarget(
                 colour=Colours.GREEN,
                 location=Coordinate(1.0, 2.0, 3.0),
@@ -25,6 +28,7 @@ class Arducam(AbstractCamera):
 
             if mapped_target is not None:
                 self.detections_logger.info(mapped_target)
+                self.detailed_detections_logger.info(mapped_target)
                 self.main_logger.info(f"Detected target: {mapped_target}")
 
         self.main_logger.info("Stopping Arducam thread.")

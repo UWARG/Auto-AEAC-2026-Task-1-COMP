f788aa3cc5b6ed1105b32cb62345efb398802f9a
honzikschenk <nojschenk@gmail.com>
2026-03-10 01:21:19 -0600

    maybe both align?

--- src/airside/detection/oakd/Basalt_VIO_RTab.py
+++ src/airside/detection/oakd/Basalt_VIO_RTab.py
@@ -33,6 +33,7 @@ def add_basalt_vio_rtab(p: dai.Pipeline, cameras: Optional[CameraBundle] = None)
         imu.setMaxBatchReports(10)
 
         stereo.setExtendedDisparity(True)
+        stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
 
         stereo.syncedLeft.link(odom.left)
         stereo.syncedRight.link(odom.right)

cfe5cd81482a555b16c8822d7e6748861d2b7c17
honzikschenk <nojschenk@gmail.com>
2026-03-10 02:15:48 -0600

    transform message removed

--- src/airside/detection/oakd/oakd.py
+++ src/airside/detection/oakd/oakd.py
@@ -116,7 +116,6 @@ class OakD(AbstractCamera):
                         time.sleep(0.01)
 
                         transform_msg = qSlamTransform.tryGet()
-                        print(f"Got transform message: {transform_msg}")
                         if isinstance(transform_msg, dai.TransformData):
                             quat = transform_msg.getQuaternion()
                             trans = transform_msg.getTranslation()
